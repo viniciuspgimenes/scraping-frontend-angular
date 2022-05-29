e6a5fc0fe92a54c8dff03e75b5db67adab15f608
Vinicius Gimenes <vinicius.pgimenes@gmail.com>
2022-05-29 20:37:30 -0300

    adding express

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CryptoPageComponent } from "./crypto-page/crypto-page.component";
 
 const routes: Routes = [
-  { path: '', component: CryptoPageComponent }
+  { path: '**', redirectTo: '' },
+  { path: '', pathMatch: 'full', component: CryptoPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
